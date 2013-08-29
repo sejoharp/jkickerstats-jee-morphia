06d5884f5b58d4b8997cb37f271419c66cc74213
joscha <zippelman@web.de>
2013-08-29 17:42:00 +0200

    couchdb config added to initialize connection

--- src/main/java/kickerstats/domain/CouchDb.java
+++ src/main/java/kickerstats/domain/CouchDb.java
@@ -1,5 +1,8 @@
 package kickerstats.domain;
 
+import java.net.MalformedURLException;
+
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.ektorp.CouchDbConnector;
@@ -8,16 +11,42 @@ import org.ektorp.http.HttpClient;
 import org.ektorp.http.StdHttpClient;
 import org.ektorp.impl.StdCouchDbInstance;
 
+@Singleton
 public class CouchDb {
 	private CouchDbInstance dbInstance;
+	private String dbname;
+	
+	private static final String DEFAULT_DB_NAME = "kickerstats";
 
-	@Singleton
-	public CouchDb() {
-		HttpClient httpClient = new StdHttpClient.Builder().build();
-		dbInstance = new StdCouchDbInstance(httpClient);		
+	@Inject
+	public CouchDb(CouchdbConfig config) {
+		if (isConfigMissing(config)) {
+			HttpClient httpClient = new StdHttpClient.Builder().build();
+			dbInstance = new StdCouchDbInstance(httpClient);
+			dbname = DEFAULT_DB_NAME;
+		} else {
+			dbInstance = createAuthenticatedDbInstance(config);
+			dbname = config.getDbname();
+		}
+	}
+
+	protected boolean isConfigMissing(CouchdbConfig config) {
+		return config.getUser() == null || config.getUser().isEmpty();
+	}
+
+	protected CouchDbInstance createAuthenticatedDbInstance(CouchdbConfig config) {
+		HttpClient authenticatedHttpClient;
+		try {
+			authenticatedHttpClient = new StdHttpClient.Builder()
+					.url(config.getUrl()).username(config.getUser())
+					.password(config.getPassword()).build();
+		} catch (MalformedURLException e) {
+			throw new IllegalArgumentException(e);
+		}
+		return new StdCouchDbInstance(authenticatedHttpClient);
 	}
 
 	public CouchDbConnector createConnection() {
-		return dbInstance.createConnector("kickerstats", false);
+		return dbInstance.createConnector(dbname, false);
 	}
 }
---
+++ src/main/java/kickerstats/domain/CouchdbConfig.java
@@ -0,0 +1,40 @@
+package kickerstats.domain;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class CouchdbConfig {
+	private String user, password, url, dbname;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDbname() {
+		return dbname;
+	}
+
+	public void setDbname(String dbname) {
+		this.dbname = dbname;
+	}
+}
