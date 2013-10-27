1fddcc5d0af5e309e68754f59d2730462d89c35c
joscha <zippelman@web.de>
2013-10-27 23:50:19 +0100

    bug fix for non starting batch

--- src/main/java/kickerstats/interfaces/StatsUpdaterBatch.java
+++ src/main/java/kickerstats/interfaces/StatsUpdaterBatch.java
@@ -1,22 +1,16 @@
 package kickerstats.interfaces;
 
-import java.util.logging.Logger;
-
 import javax.ejb.Schedule;
+import javax.ejb.Stateful;
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
-@Singleton
+@Stateful
 public class StatsUpdaterBatch {
-	private static Logger LOG = Logger.getLogger(StatsUpdaterBatch.class
-			.getName());
-
 	@Inject
 	private StatsUpdater statsUpdater;
 
 	@Schedule(dayOfMonth = "*")
 	public void schedule() {
 		statsUpdater.updateStats();
-		LOG.info("updater batch scheduled.");
 	}
 }
